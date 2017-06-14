76422b09db5a4b87c236714a1113c298d959e211
Karol Stępień <karol.stepien@me.com>
2017-06-14 15:13:18 +0200

    Persistence Manager, storyboards

---
+++ Careo/Other/UIConstants.swift
@@ -0,0 +1,8 @@
+// Auto generated file from SBConstants - any changes may be lost
+
+public enum StoryboardNames : String {
+    case CarDetails
+    case CarForm
+    case CarList
+    case LaunchScreen
+}
