3a9760902500b61c0d02928467c38cbd46589bd0
Karol Stępień <karol.stepien@me.com>
2017-06-14 14:08:06 +0200

    project init, basic file hierarchy, gems init, pods init

---
+++ Careo/CarListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CarListViewController.swift
+//  Careo
+//
+//  Created by Karol Stępień on 14.06.2017.
+//  Copyright © 2017 carlst. All rights reserved.
+//
+
+import UIKit
+
+class CarListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
